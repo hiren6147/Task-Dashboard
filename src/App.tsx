67197fc457af365d0707c5e0382d1ff6d230abe3
hiren2.patel <hiren2.patel@paytm.com>
2025-03-06 08:26:45 +0530

    Add ErrorBoundary component to handle errors in the application

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import { store, persistor } from "@/store/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { Toaster } from "@/components/ui/sonner";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-          <Homepage />
-        </ThemeProvider>
-        <Toaster />
+        <ErrorBoundary>
+          <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <Homepage />
+          </ThemeProvider>
+          <Toaster />
+        </ErrorBoundary>
       </PersistGate>
     </Provider>
   );
